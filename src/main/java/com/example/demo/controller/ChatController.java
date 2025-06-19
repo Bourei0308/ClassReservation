f293dc3261470da0f39c7ff5107dca6ca740b516
luyingjia <luyingjia@ia.com>
2025-06-19 13:54:02 +0900

    🐇コントローラーの作成

---
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Chat;
+import com.example.demo.repository.ChatRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/chats")
+@Tag(name = "Chat API", description = "チャットAPI")
+public class ChatController {
+	@Autowired
+	private ChatRepository repository;
+
+	@GetMapping("/user/{userId}")
+	@Operation(summary = "全てのチャット履歴取得")
+	public List<Chat> getUserChats(@PathVariable String userId) {
+		return repository.findByFromUserIdOrToUserId(userId, userId);
+	}
+
+	@PostMapping
+	@Operation(summary = "チャット追加")
+	public Chat sendMessage(@RequestBody Chat chat) {
+		return repository.save(chat);
+	}
+}
