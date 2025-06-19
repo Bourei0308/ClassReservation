982607d99b43988917c87ef58f2d5465ee13df81
butty <kota.butsusaka@gigxit.co.jp>
2025-06-19 17:17:28 +0900

    🐬 add: calendarを実装

---
+++ ClassReservationVue/src/views/general_user/student_top.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Top_Calender></Top_Calender>
+    </div>
+</template>
+
+<script setup>
+    import { useRouter } from 'vue-router'; // Vue RouterのuseRouterフックをインポート
+    import { ref , onMounted} from 'vue'; // VueのrefをインポートClassReservationVue\src\components\top_calendar.vue
+    import Top_Calender from './../../components/top_calendar.vue'; // 子コンポーネントをインポート
+    const router = useRouter(); // useRouterフックを使用してルーターインスタンスを取得
+</script>
