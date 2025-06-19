6d46535ce6e435af647ed93fb70589c317172204
yang <2430371802@qq.com>
2025-06-19 16:09:45 +0900

    🐑header footer

---
+++ ClassReservationVue/src/components/sitefooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="site_footer">
+    11@22
+  </footer>
+</template>
+
+<script setup>
+// 无需脚本
+</script>
+
+<style>
+.site_footer {
+  background-color: black;
+  color: white;
+  font-size: 12px;
+  text-align: center;
+  padding: 24px 0;
+}
+</style>
