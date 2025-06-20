60c3ac49b343f0423d432cb347ddf2499482c985
butty <kota.butsusaka@gigxit.co.jp>
2025-06-20 13:50:24 +0900

    🐬 fix:APIをさらに修正

--- src/main/java/com/example/demo/repository/AvailableTimeRepository.java
+++ src/main/java/com/example/demo/repository/AvailableTimeRepository.java
@@ -3,10 +3,17 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.entity.AvailableTime;
 
 public interface AvailableTimeRepository extends MongoRepository<AvailableTime, String> {
 	List<AvailableTime> findByTeacherId(String teacherId);
-    List<AvailableTime> findByTeacherIdAndYearAndMonth(String teacherId, String year, String month);
+    @Query("SELECT a FROM AvailableTime a WHERE a.teacherId = :teacherId AND FUNCTION('YEAR', a.date) = :year AND FUNCTION('MONTH', a.date) = :month")
+    List<AvailableTime> findByTeacherIdAndYearAndMonth(
+            @Param("teacherId") String teacherId,
+            @Param("year") String year,
+            @Param("month") String month
+    );
 }
