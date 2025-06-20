fdf8a8fbe8253ae5b5e6536cfa8f89e66465da6d
yang <2430371802@qq.com>
2025-06-20 13:26:31 +0900

    🐑　topchatbox

--- ClassReservationVue/src/views/general_user/student_top.vue
+++ ClassReservationVue/src/views/general_user/student_top.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <Top_Calender></Top_Calender>
+        <Top_Calender />
+        <Top_ChatBox />
     </div>
 </template>
 
 <script setup>
     import { useRouter } from 'vue-router'; // Vue RouterのuseRouterフックをインポート
     import { ref , onMounted} from 'vue'; // VueのrefをインポートClassReservationVue\src\components\top_calendar.vue
-    import Top_Calender from './../../components/top_calendar.vue'; // 子コンポーネントをインポート
+    import Top_Calender from '@/components/top_calendar.vue'; // 子コンポーネントをインポート
+    import Top_ChatBox from '@/components/top_chatbox.vue'; // 子コンポーネントをインポート
     const router = useRouter(); // useRouterフックを使用してルーターインスタンスを取得
 </script>
